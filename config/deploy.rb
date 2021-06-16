fab3cfde24a29cc308ed852eb7671ce04317332c
ShoheiTsuji-CRE <shohei.tsuji@mixi.co.jp>
2021-06-16 12:56:28 +0900

    credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,3 +16,5 @@ set :rbenv_ruby, '2.6.3'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
+
+append :linked_files, 'config/master.key'
